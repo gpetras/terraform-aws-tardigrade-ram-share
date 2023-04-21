ef67c96e327a77522f3f3f6ff5234e078e47405b
Greg Petras <greg.petras@trove.co>
2023-04-21 16:27:11 -0700

    add time_sleep

--- modules/principal_association/main.tf
+++ modules/principal_association/main.tf
@@ -1,9 +1,20 @@
 resource "aws_ram_principal_association" "this" {
   principal          = var.principal
   resource_share_arn = var.resource_share_arn
+}
 
-  # The invitation sometime takes a few seconds to propagate
-  provisioner "local-exec" {
-    command = "python -c 'import time; time.sleep(10)'"
+#resource "time_sleep" "10s_wait" {
+#  depends_on = [aws_ram_principal_association.this]
+#
+#  create_duration = "10s"
+#}
+
+resource "time_sleep" "ram_resource_propagation" {
+  create_duration = "10s"
+
+  triggers = {
+    # This sets up a proper dependency on the RAM association
+    resource_share_arn = var.resource_share_arn
+    principal  = var.principal
   }
 }
